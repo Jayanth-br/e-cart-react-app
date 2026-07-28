4f43aef8b1b058e77b5e7c2d741c2708b99a5c3e
jayanth <jayanth.br@evoluteiq.com>
2026-07-28 22:33:58 +0530

    Home screen product card design

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -1,6 +1,8 @@
 import NavBar from "../NavBar";
-import products from "../../util/products.json"
-import "./Home.css"
+import products from "../../util/products.json";
+import "./Home.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 function Home() {
     return (
         <>
@@ -11,8 +13,14 @@ function Home() {
                     {products.map((product) => {
                         return <li className="product" key={product.id}>
                             <img src={product.image} alt={product.title} height={220} width={220}/>
-                            <h4>{product.title}</h4>
-                            <p>₹{product.price.toLocaleString('en-IN')}</p>
+                            <h3 className="product-title">{product.title}</h3>
+                            <p className="product-info">{product.info}</p>
+                            <p className="product-rating">
+                                <FontAwesomeIcon icon={faStar} style={{color: "#255fa6",}} />
+                                {product.rating}  ({product.reviews})</p>
+                            <p className="product-price">
+                                ₹{product.price.toLocaleString('en-IN')}</p>
+                            <button className="add-to-cart-btn">Add to Cart</button>
                         </li>
                     })}
                 </ul>
