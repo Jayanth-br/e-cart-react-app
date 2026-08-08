46ed01fe76815d7995ec21002572d2cdf75b1099
Jayanth <jayanthbr2000@gamil.com>
2026-08-08 18:15:33 +0530

    1. Added ProductDetail & Product page 2. Added routing for Product page.

---
+++ src/Components/ProductDetail.jsx
@@ -0,0 +1,7 @@
+function ProductDetail() {
+
+    return (
+        <h1>Product</h1>
+    )
+}
+export default ProductDetail;
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './Components/pages/Home.jsx'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Product from './Components/pages/Product.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -10,7 +11,9 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Navigate to="/home" replace/>}/>
-        <Route path="/home" element={<Home />}/>
+        <Route path='/home' element={<Home />}/>
+
+        <Route path='/product' element={<Product />}/>
       </Routes>
     </BrowserRouter>
     
