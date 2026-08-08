7642a97fbe16f6e432a23c3ca4ceabb1e75a2254
Jayanth <jayanthbr2000@gamil.com>
2026-08-08 17:32:20 +0530

    Added default routing "/" to "/home"

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './Components/pages/Home.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path='/' element={<Navigate to="/home" replace/>}/>
+        <Route path="/home" element={<Home />}/>
       </Routes>
     </BrowserRouter>
     
