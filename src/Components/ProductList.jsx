8347e75fb269e18993a0e117f591980c422f0bd2
Jayanth <jayanthbr2000@gamil.com>
2026-08-11 21:58:15 +0530

    Product color selector

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -8,7 +8,6 @@ function ProductCard({title, products}) {
 
     function onClickProduct(product){
         navigate(`/product/${product.id}`);
-        
     }
 
     return (
@@ -18,16 +17,32 @@ function ProductCard({title, products}) {
             }
             <ul className="product-list">
                 {products.map((product) => (
-                    <li className="product-card" key={product.id} onClick={() => onClickProduct(product)}>
-                        <img src={product.image} alt={product.title} height={220} className="product-img"/>
+                    <li 
+                        className="product-card" 
+                        key={product.id} 
+                        onClick={() => onClickProduct(product)}>
+
+                        <img 
+                            src={product.image} 
+                            alt={product.title} 
+                            height={220} 
+                            className="product-img"/>
+
                         <h3 className="product-title">{product.title}</h3>
                         <p className="product-info">{product.info}</p>
-                        <p className="product-rating">
-                            <FontAwesomeIcon icon={faStar} style={{color: "#255fa6",}} />
+                        <p 
+                            className="product-rating">
+                            <FontAwesomeIcon icon={faStar} 
+                            style={{color: "#255fa6"}} 
+                        />
                             {product.rating}  ({product.reviews})</p>
                         <p className="product-price">
                             ₹{product.price.toLocaleString('en-IN')}</p>
-                        <button className="add-to-cart-btn">Add to Cart</button>
+
+                        <button 
+                            className="add-to-cart-btn">
+                            Add to Cart
+                        </button>
                     </li>
                 ))}
             </ul>
